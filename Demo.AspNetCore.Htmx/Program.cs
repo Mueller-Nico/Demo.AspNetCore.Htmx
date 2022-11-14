073ee047cf16964da360c8092a6df0226a828014
Mueller-Nico <nicolaus.mueller@t-online.de>
2022-11-14 12:38:35 +0100

    EF Core SQLite

--- Demo.AspNetCore.Htmx/Program.cs
+++ Demo.AspNetCore.Htmx/Program.cs
@@ -1,4 +1,10 @@
+using Demo.AspNetCore.Htmx.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
