76fbd876d10f81456382fe34a62a5de0470003d6
Mueller-Nico <nicolaus.mueller@t-online.de>
2022-11-14 12:24:33 +0100

    Add project files.

---
+++ Demo.AspNetCore.Htmx/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Demo.AspNetCore.Htmx.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo.AspNetCore.Htmx.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo.AspNetCore.Htmx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demo.AspNetCore.Htmx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
